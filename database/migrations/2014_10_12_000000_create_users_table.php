7871608db5de9cbb465e6fd12de6d62d0f2421c7
funayamateppei <t.funayama0217@gmail.com>
2023-03-08 10:23:13 +0900

    Userのマイグレーションファイルとモデル編集

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,12 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('icon_path')->nullable();
+            $table->string('address')->nullable();
+            $table->string('height')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('line_id')->nullable();
+            $table->string('instagram_id')->nullable();
             $table->timestamps();
         });
     }
