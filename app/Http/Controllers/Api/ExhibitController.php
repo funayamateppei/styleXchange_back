fddde2ebcd332a9c8bcea7484b18967376624da5
funayamateppei <t.funayama0217@gmail.com>
2023-03-20 17:07:25 +0900

    threadsに保存したデータと紐づけて、thread_imagesをforeach文で保存実装

--- app/Http/Controllers/Api/ExhibitController.php
+++ app/Http/Controllers/Api/ExhibitController.php
@@ -11,7 +11,6 @@ use App\Models\ThreadImage;
 use App\Models\Item;
 use App\Models\ItemImage;
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class ExhibitController extends Controller
@@ -32,18 +31,30 @@ class ExhibitController extends Controller
 
         // thread_imagesに保存する情報
         $threadImages = $request->file('threadImages');
+        foreach ($threadImages as $threadImage) {
+            $filename = uniqid() . '.' . $threadImage->getClientOriginalExtension(); // 画像ファイルにユニークな名前をつける + 拡張子
+            $store = $threadImage->storeAs('storage/thread_images', $filename, 'public');
+            $path = '/' . $store;
+            $threadImageData = [
+                'thread_id' => $threadResponse->id,
+                'path' => $path,
+                'original_file_name' => $filename,
+            ];
+            $threadImagesResponse = ThreadImage::create($threadImageData);
+        }
 
         // itemsとitem_imagesに保存する情報
-        $items = $request->input('items');
-        $itemImages = $request->file('items');
-        foreach ($items as $key => $item) { // $itemsにpostで送られてきたファイルを格納
-            $items[$key]['images'] = $itemImages[$key]['images'];
-        }
+        // $items = $request->input('items');
+        // $itemImages = $request->file('items');
+        // foreach ($items as $key => $item) { // $itemsにpostで送られてきたファイルを格納
+        //     $items[$key]['images'] = $itemImages[$key]['images'];
+        // }
 
 
 
         // Log::debug($thread); // 確認用
-        Log::debug($threadResponse); // 確認用
+        // Log::debug($threadResponse); // 確認用
+        Log::debug($threadImagesResponse); // 確認用
         // Log::debug($threadImages); // 確認用
         // Log::debug($items); // 確認用
     }
