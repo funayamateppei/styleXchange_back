96924ea32f12479da8e9bd25481b2673c085b701
funayamateppei <t.funayama0217@gmail.com>
2023-03-23 20:19:42 +0900

    threadのcomment一覧を取得する処理作成

--- app/Http/Controllers/Api/ThreadController.php
+++ app/Http/Controllers/Api/ThreadController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Thread;
+use App\Models\ThreadComment;
 
 class ThreadController extends Controller
 {
@@ -18,8 +19,23 @@ class ThreadController extends Controller
     public function getThread(Request $request)
     {
         $id = $request['id'];
-        $data = Thread::with(['user', 'items.itemImages', 'threadImages', 'threadComments', 'likedThreads', 'bookmarkedThreads'])
+        $data = Thread::with(['user.threads.threadImages', 'items.itemImages', 'threadImages', 'threadComments.user', 'likedThreads', 'bookmarkedThreads'])
             ->find($id);
+        return response()->json($data);
+    }
+
+    public function getThreadComments(Request $request)
+    {
+        $id = $request['id'];
+        $threadData = Thread::find($id);
+        $commentData = ThreadComment::with('user')
+            ->where('thread_id', $id)
+            ->orderBy('created_at', 'desc')
+            ->get();
+        $data = [
+            'threadData' => $threadData,
+            'commentData' => $commentData,
+        ];
         return response()->json($data);
     }
 }
